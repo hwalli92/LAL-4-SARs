c935d1bfe8e3cf299ad47816881ec758326bf70b
hwalli92 <62026003+hwalli92@users.noreply.github.com>
2022-04-19 16:25:57 +0200

    rework data processor to dynamically adjust body number

--- data/process_skel_data.py
+++ data/process_skel_data.py
@@ -11,39 +11,42 @@ def find_all(action, path):
     return result
 
 
-def extract_skel_data(data, body_data):
-    body_data["nframes"] = int(data[0].strip("\r\n"))
-    body_data["nbodies"] = []
-    body_data["njoints"] = int(data[3].strip("\r\n"))
-    body_data["skel_body"] = np.zeros(
-        (body_data["nframes"], body_data["njoints"], 3), dtype=np.float32
-    )
-    body_data["rgb_body"] = np.zeros(
-        (body_data["nframes"], body_data["njoints"], 2), dtype=np.float32
-    )
-    body_data["depth_body"] = np.zeros(
-        (body_data["nframes"], body_data["njoints"], 2), dtype=np.float32
-    )
+def extract_skel_data(data):
+
+    num_frames = int(data[0].strip("\r\n"))
+    num_bodies = []
+    bodies_data = []
+
+    for b in range(4):  # max bodies
+        body_data = dict()
+        body_data["skel_body"] = np.zeros((num_frames, 25, 3), dtype=np.float32)
+        body_data["rgb_body"] = np.zeros((num_frames, 25, 2), dtype=np.float32)
+        body_data["depth_body"] = np.zeros((num_frames, 25, 2), dtype=np.float32)
+
+        bodies_data.append(body_data)
 
     line = 0
 
-    for frame in range(body_data["nframes"]):
+    for frame in range(num_frames):
         line += 1
-        bodycount = int(data[line][:-1])
-        print(line, bodycount)
-        if bodycount == 0:
-            print("Empty Frame")
+        body_count = int(data[line][:-1])
+
+        if body_count == 0:
             continue
 
-        for b in range(body_data["nbodies"]):
+        num_bodies.append(body_count)
+
+        for body in range(body_count):
+            body_data = bodies_data[body]
             line += 1
-            bodyinfo = data[line][:-1].split(' ')
-            print(line, bodyinfo)
+            body_info = data[line][:-1].split(" ")
+
             line += 1
-            for j in range(body_data["njoints"]):
+            num_joints = int(data[line].strip("\r\n"))
+
+            for joint in range(num_joints):
                 line += 1
                 temp_data = data[line].strip("\r\n").split()
-                print(line, temp_data)
                 body_data["skel_body"][frame, j] = np.array(
                     temp_data[:3], dtype=np.float32
                 )
@@ -54,7 +57,13 @@ def extract_skel_data(data, body_data):
                     temp_data[3:5], dtype=np.float32
                 )
 
-    return body_data
+            bodies_data[body] = body_data
+
+    for b in range(4):
+        if b >= max(num_bodies):
+            bodies_data.pop(b)
+
+    return num_frames, max(num_bodies), bodies_data
 
 
 if __name__ == "__main__":
@@ -65,21 +74,21 @@ if __name__ == "__main__":
 
     for idx, file in enumerate(datalist):
         print(idx, file)
-        body_data = dict()
+        data = dict()
 
         with open(path + file, "r") as fr:
             str_data = fr.readlines()
 
-        body_data["setup"] = int(file[1:4])
-        body_data["camera"] = int(file[5:8])
-        body_data["subject"] = int(file[9:12])
-        body_data["replication"] = int(file[13:16])
-        body_data["action"] = int(file[17:20])
+        data["setup"] = int(file[1:4])
+        data["camera"] = int(file[5:8])
+        data["subject"] = int(file[9:12])
+        data["replication"] = int(file[13:16])
+        data["action"] = int(file[17:20])
 
-        body_data = extract_skel_data(str_data, body_data)
+        data["nframes"], data["nbodies"], data["bodies_data"] = extract_skel_data(
+            str_data
+        )
 
-        action_data.append(body_data)
-
-
-    with open("raw_data/A001.pkl", 'wb') as fw:
+        action_data.append(data)
+    with open("raw_data/A001.pkl", "wb") as fw:
         pickle.dump(action_data, fw, pickle.HIGHEST_PROTOCOL)
